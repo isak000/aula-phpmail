a5621ef1e943093f94835872c0278d5522003fed
isaque <isaqueemanoel21@gmail.com>
2022-12-07 10:22:47 -0300

    testando

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="insert.php" method="post">
+    <label for="text">digite um nome</label>
+    <input type="text" name="nome">
+    <br>
+    <label for="text">digite um email</label>
+    <input type="email" name="email">
+    <br>
+    <input type="submit" placeholder="emviar">
+
+    </form>
+</body>
+</html>
